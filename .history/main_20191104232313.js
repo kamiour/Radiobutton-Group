18463fc0567873bdcb2ec4ae5f212c1783174b70
Mykhailo Kazakov <kamiournew@gmail.com>
2019-11-05 00:17:48 +0200

    deleted .history

--- .history/main_20191104232313.js
+++
@@ -1,8 +0,0 @@
-let radioArr = $(".contact-form-radio");
-for (let i = 0; i < radioArr.length; i++) {
-	if (radioArr[i]:checked) {
-  	for (let j = 0; j < i; j++) {
-    	radioArr[j].style.backgroundColor = 'pink';
-    }
-  }
-}
--- .history/main_20191104233147.js
+++
@@ -1,9 +0,0 @@
-let radioArr = $(".contact-form-radio");
-let lineArr = $(".check-line");
-for (let i = 0; i < radioArr.length; i++) {
-	if (radioArr[i].checked) {
-  	for (let j = 0; j <= i; j++) {
-    	radioArr[j].style.backgroundColor = 'pink';
-    }
-  }
-}
